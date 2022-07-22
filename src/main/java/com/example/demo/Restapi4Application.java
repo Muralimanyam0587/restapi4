87bb3507813723969f60836fe4561f07e0af64fd
murali <manyam123murali@gmail.com>
2022-07-22 10:21:16 +0530

    firsrt commit

---
+++ src/main/java/com/example/demo/Restapi4Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Restapi4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Restapi4Application.class, args);
+	}
+
+}
